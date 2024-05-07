9135a229a25f33c1054519ab11f660dae16cd020
Romera02rrs <romera02rrs@gmail.com>
2024-05-07 20:26:40 +0200

    Tracking complete, commit.

--- ai_mosue_hand_tracker.py
+++ ai_mosue_hand_tracker.py
@@ -17,7 +17,7 @@ clocX, clocY = 0, 0
 cap = cv2.VideoCapture(0)
 cap.set(3, wCam)
 cap.set(4, hCam)
-detector = htm.handDetector(maxHands=1, detectionCon=1)
+detector = htm.handDetector(maxHands=4, detectionCon=1)
 wScr, hScr = autopy.screen.size()
 # print(wScr, hScr)
 
